ede95a6de114994a9b958ba5c9b5675626f25b59
marsbearmc <melchi@garfias.org>
2021-07-04 05:07:18 -0700

    more work

--- bot.js
+++ bot.js
@@ -1,11 +1,13 @@
 require('dotenv').config();
 
-const { Client, Message } = require('discord.js');
+const config = require('./config.json')
 
+const { Client, Message } = require('discord.js');
 const client = new Client();
 
-client.login(process.env.DISCORDJS_BOT_TOKEN);
+client.login(config.token);
 
 client.on('ready', () => {
-    console.log("Bot Logged In")
-})
+    console.log("Bot Logged In");
+});
+
